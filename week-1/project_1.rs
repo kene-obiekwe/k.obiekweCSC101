79e1957c63eeffde6e89ccffbedc289881c46824
kene <kene.obiekwe@pau.edu.ng>
2022-10-22 17:30:05 +0100

    (project_1.rs)

--- week-1/project_1.rs
+++ week-1/project_1.rs
@@ -3,7 +3,9 @@ fn main () {
     let r = 10.00;
     let n = 5.00;
     let radical = 1.00+r/100.00;
+    // f32 is used for floating point numbers
     let radical = radical as f32;
+    // powf is used to raise floating point numbers to a certain power
     let a = p * radical.powf(n);
     println!("Amount is equal to {}",a );
     let ci = a - p;
