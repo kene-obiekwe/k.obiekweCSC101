10e17b8e4c544402393f6773e721d0a0025076ad
kene <kene.obiekwe@pau.edu.ng>
2022-10-22 17:08:10 +0100

    (project_2.rs)

---
+++ week-1/project_2.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let toshiba = 450000.00 * 2.00;
+    let mac = 1500000.00 * 1.00;
+    let hp = 750000.00 * 3.00;
+    let dell = 2850000.00 * 3.00;
+    let acer = 250000.00 * 1.00;
+    let sum = toshiba + mac + hp + dell + acer;
+    println!("Sum is equal to {}",sum );
+    let average = sum / 10.00;
+    println!("Average is equal to {}",average );
+}    
